028cb8817002d718b0e66aeae723b87c8b462b76
Mehmet Varol <mhmtvrl1731@gmail.com>
2015-08-05 00:00:21 -0400

    Problem #203 class is added

---
+++ src/LinkedList/RemoveLinkedListElements.java
@@ -0,0 +1,29 @@
+package LinkedList;
+/**
+ * Problem number 	: 203
+ * Problem url		: https://leetcode.com/problems/remove-linked-list-elements/
+ * 
+ * Remove all elements from linkedlistof integers that have value val.
+ * 
+ * Example
+ * 
+ * Given: 1->2->6->3->4->5->6, val = 6
+ * 
+ * Return: 1->2->3->4->5
+ * @author mehmet
+ *
+ */
+
+public class RemoveLinkedListElements {
+	
+	public class ListNode{
+		int val;
+		ListNode next;
+		ListNode(int x){ val = x; }
+	}
+	
+	public ListNode removeElements(ListNode head, int val){
+		return null;
+	}
+
+}
