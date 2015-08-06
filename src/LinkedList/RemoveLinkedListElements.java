8d225bfecf22f12bdad22ed328a5db49ec307dfc
Mehmet Varol <mhmtvrl1731@gmail.com>
2015-08-05 22:38:14 -0400

    Problem #203 is solved

--- src/LinkedList/RemoveLinkedListElements.java
+++ src/LinkedList/RemoveLinkedListElements.java
@@ -22,8 +22,52 @@ public class RemoveLinkedListElements {
 		ListNode(int x){ val = x; }
 	}
 	
-	public ListNode removeElements(ListNode head, int val){
-		return null;
-	}
+	/**
+	 * Deletion :
+	 * 	1.)store current note to temp
+	 * 	2.)move current to next node
+	 *  3.)point prev.next to current
+	 *  4.)point temp.next to null
+	 *  
+	 * Temp node is deleted node. If we don't dereference deleted note garbage
+	 * collector doesn't collect it and it become memory leak.
+	 * 
+	 * TODO Refactor
+	 * TODO Performance is average
+	 * @param head
+	 * @param val
+	 * @return
+	 */
+	public ListNode removeElements(ListNode head, int val) {
+        if(head == null)
+            return null;
+        
+        ListNode current = head;
+        ListNode previous = null;
+        
+        while(current != null){
+            if(current.val == val) {
+                ListNode temp = current;
+                current = current.next;
+                
+                //When we delete first node it references null so it doesn't have next
+                if(previous != null)
+                	//condition for non-first node
+                    previous.next = current;
+                else
+                	//Head shows first node so when we delete first node
+                	//we should change the head
+                    head = current;
+                temp.next = null;
+            }
+            //We moved pointers when we deleted the node so we don't need to move them again
+            //but if we don't delete any node in the iteration we should move pointers.
+            else {
+                previous = current;
+                current = current.next;
+            }
+        }
+        return head;
+    }
 
 }
